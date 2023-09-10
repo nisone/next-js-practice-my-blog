389198db3db64b2aa0200f60aaaac1fd2e8a1cf0
nisone <nuhu94@gmail.com>
2023-09-11 00:38:43 +0100

    update ui design and code refactor

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,7 @@
-import { profile } from "console";
-import { revalidatePath, revalidateTag } from "next/cache";
-import { json } from "stream/consumers";
-
-export interface Product {
-  id?: string;
-  name?: String;
-  price?: String;
-}
+import { addToDatabase, removeFromDatabase } from "@/actions/products";
+import { Product } from "@/typings";
+import Link from "next/link";
+import Image from "next/image";
 
 export default async function Dashboard() {
 
@@ -20,65 +15,46 @@ export default async function Dashboard() {
 
   const products: Product[] = await res.json()
 
-  const addToDatabase = async (e: FormData) => {
-    'use server'
-
-    const name = e.get('name')?.toString()
-    const price = e.get('price')?.toString()
-
-    const product: Product = {
-      name,
-      price
-    }
-
-    console.log(product)
-
-    const res = await fetch('https://64fe2561596493f7af7ef302.mockapi.io/api/v1/products', {
-      method: 'POST',
-      body: JSON.stringify(product)
-    })
-
-    // revalidateTag('products')
-    revalidatePath('/dashboard')
-  }
-
-  const removeFromDatabase = async (e: FormData) => {
-    'use server'
-
-    const id = e.get('id')?.toString()
-
-    const res = await fetch('https://64fe2561596493f7af7ef302.mockapi.io/api/v1/products/' + id, {
-      method: 'DELETE',
-    })
-
-    revalidateTag('products')
-  }
-
   return (
-    <main>
-      <form action={addToDatabase} className="flex flex-col gap-5 max-w-sm mx-auto">
-        <input type="text" name="name" placeholder="name" />
-        <input type="text" name="price" placeholder="price" />
-        <button type="submit">Add product</button>
-      </form>
+    <div>
+      <h1 className="text-3xl font-bold text-center py-5">Product page</h1>
+      <div className="flex md:flex-row flex-col-reverse gap-5">
+        <div className="p-10 shadow-md rounded-md bg-gray-100">
+          <h1 className="text-3xl font-bold text-center py-2">Product List</h1>
+          <div className="grid lg:grid-cols-4 grid-cols-2 gap-5 mx-auto py- 5">
+            {
+              products.map((e) => (
+                <div key={e?.id} className="flex flex-col gap-2 p-2 shadow-md rounded-md bg-gray-50">
+                  <Image className="object-cover h-48 w-96" src={'https://loremflickr.com/640/480/fashion'} width={100} height={100} alt='product image'></Image>
+                  <h1>{e.name}</h1>
+                  <h1>{e.price}</h1>
+                  <div className="flex flex-row gap-2">
+                    <Link href={'/dashboard/product/' + e?.id} className="btn bg-gray-600 text-white p-1 mx-auto rounded-md">Update</Link>
+                    <form action={removeFromDatabase}>
+                      <input type="hidden" name="id" value={e?.id} />
+                      <button type="submit" className="btn bg-red-600 text-white p-1 mx-auto rounded-md">Delete</button>
+                    </form>
+                  </div>
 
-      <h1> Protected Page</h1>
-      {
-        products.map((e) => (
-          <div key={e?.id} className="flex flex-wrap gap-5 mx-auto py- 5">
-            <div className="flex flex-col gap-2">
-              <h1>{e.name}</h1>
-              <h1>{e.price}</h1>
-              <form action={removeFromDatabase}>
-                <input type="hidden" name="id" value={e?.id} />
-                <button type="submit" className="btn bg-red-600 p-1 mx-auto">Delete</button>
-              </form>
 
-            </div>
+                </div>
 
+              ))
+            }
           </div>
-        ))
-      }
-    </main>
+        </div>
+
+        <div className="p-10 shadow-md rounded-md bg-gray-100">
+          <h1 className="text-3xl font-bold text-center py-2">Add product</h1>
+          <form action={addToDatabase} className="flex flex-col gap-5 max-w-sm mx-auto">
+            <input type="text" name="name" placeholder="name" className="border border-blue-300 p-2 rounded-md" />
+            <input type="text" name="price" placeholder="price" className="border border-blue-300 p-2 rounded-md" />
+            <button type="submit" className="btn bg-blue-400 text-white p-1 rounded-md">Add product</button>
+          </form>
+        </div>
+
+      </div>
+
+    </div>
   );
 };
--- next.config.js
+++ next.config.js
@@ -2,7 +2,17 @@
 const nextConfig = {
   experimental: {
     serverActions: true
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'loremflickr.com',
+        port: '',
+        pathname: '/640/480/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
